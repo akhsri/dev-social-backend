99f361b21990444d0ba7ee8793c6bb3ac121314f
Aakash Srivastav <akhsri21@gmail.com>
2021-03-28 01:28:37 +0530

    Add: user routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -67,6 +67,11 @@ exports.signout = (req, res) => {
     })
 }
 
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+})
+
 exports.isAuthenticated = (req, res, next) => {
     let checker = req.profile && req.auth && req.profile._id == req.auth._id;
     if (!checker) {
