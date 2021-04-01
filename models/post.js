2ebcba7783a90162ded4fca36290d4912043348a
Aakash Srivastav <akhsri21@gmail.com>
2021-04-01 13:36:25 +0530

    Add: post model

---
+++ models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
+const postSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        require: true,
+    },
+    photo: {
+        data: Buffer,
+        contentType: String,
+    },
+    likes: [{
+        type: ObjectId,
+        ref: 'User'
+    }],
+    comments: [{
+        text: String,
+        postedBy: {
+            type: object,
+            ref: 'User'
+        }
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
