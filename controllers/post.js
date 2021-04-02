90cf7d1fa58c107b36faa7a2a48ba30c3dc72478
Aakash Srivastav <akhsri21@gmail.com>
2021-04-02 15:57:28 +0530

    Add: deletePost, updatePost and getAllPosts controller

--- controllers/post.js
+++ controllers/post.js
@@ -87,3 +87,76 @@ exports.deletePost = (req, res) => {
         })
     })
 }
+
+exports.deletePost = (req, res) => {
+    let post = req.post;
+    post.remove((err, deletedPost) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Failed to delete the post"
+            })
+        }
+        res.json({
+            message: "Post deleted successfully"
+        })
+    })
+}
+
+// Update Controller
+exports.updatePost = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "problem with image"
+            });
+        }
+        //console.log("files", files)
+
+
+        // Updation code
+        let post = req.post;
+        post = _.extend(post, fields)
+
+        // handle file here
+        console.log("file.photo", files.photo);
+        if (files.photo) {
+            if (files.photo.size > 5000000) {
+                return res.status(400).json({
+                    error: "File size too big!"
+                });
+            }
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+
+        post.save((err, post) => {
+            if (err || !post) {
+                res.status(400).json({
+                    error: "Updation of post failed"
+                });
+            }
+            res.json(post);
+        });
+
+    });
+}
+
+exports.getAllPosts = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 8;
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    Product.find()
+        .select("-photo")
+        .sort([[sortBy, "asc"]])
+        .limit(limit)
+        .exec((err, posts) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "No post found"
+                })
+            }
+            res.json(posts)
+        })
+}
